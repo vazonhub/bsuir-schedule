9b279aa907f65ac0b6216edade92379952a5d988
Kostya Betenya <betenya.kostya@mail.ru>
2026-07-19 12:38:14 +0300

    chore(lint): migrate eslint config to flat format for eslint 9

---
+++ eslint.config.js
@@ -0,0 +1,54 @@
+const { FlatCompat } = require('@eslint/eslintrc');
+const js = require('@eslint/js');
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+});
+
+module.exports = [
+  {
+    ignores: [
+      'node_modules/',
+      '.expo/',
+      'dist/',
+      'ios/',
+      'android/',
+      'babel.config.js',
+      'eslint.config.js',
+      'services/',
+      'scripts/',
+    ],
+  },
+  ...compat.config({
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      ecmaFeatures: { jsx: true },
+    },
+    env: {
+      'react-native/react-native': true,
+      es2022: true,
+      node: true,
+    },
+    plugins: ['@typescript-eslint', 'react', 'react-hooks', 'react-native', 'prettier'],
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:react/recommended',
+      'plugin:react-hooks/recommended',
+      'plugin:react-native/all',
+      'prettier',
+    ],
+    settings: { react: { version: 'detect' } },
+    rules: {
+      'prettier/prettier': 'warn',
+      'react/react-in-jsx-scope': 'off',
+      'react-native/no-raw-text': 'off',
+      'react-native/sort-styles': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/consistent-type-imports': 'warn',
+    },
+  }),
+];
